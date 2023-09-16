e04df60416b4edececf482d2bfacad5434f01b9c
MinseoShindor <tlsalstj01@duksung.ac.kr>
2023-09-17 07:23:04 +0900

    Fix: navigation graph Logic

--- app/src/main/java/com/minseo/assginmentkakao/HomeFragment.kt
+++ app/src/main/java/com/minseo/assginmentkakao/HomeFragment.kt
@@ -3,21 +3,35 @@ package com.minseo.assginmentkakao
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.onNavDestinationSelected
 import com.minseo.assginmentkakao.common.BaseAppFragment
 import com.minseo.assginmentkakao.databinding.FragmentHomeBinding
 
 
 class HomeFragment : BaseAppFragment<FragmentHomeBinding>() {
+
+    private lateinit var navController: NavController
+
+
     override fun getFragmentBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentHomeBinding = FragmentHomeBinding.inflate(inflater)
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        navController = Navigation.findNavController(view)
 
     }
+
+
+
 
 }
--- app/src/main/java/com/minseo/assginmentkakao/MainActivity.kt
+++ app/src/main/java/com/minseo/assginmentkakao/MainActivity.kt
@@ -2,17 +2,25 @@ package com.minseo.assginmentkakao
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout.Directions
+import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.activity.viewModels
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
 import com.minseo.assginmentkakao.databinding.ActivityMainBinding
+import com.minseo.assginmentkakao.navigation.AddNavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -20,6 +28,9 @@ class MainActivity : AppCompatActivity() {
     private val binding get() = _binding!!
 
     private lateinit var toolbar: Toolbar
+    private val navController by lazy {
+        (supportFragmentManager.findFragmentById(R.id.main_fcv) as NavHostFragment).navController
+    }
     private lateinit var inflater: MenuInflater
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,6 +49,12 @@ class MainActivity : AppCompatActivity() {
         inflater.inflate(R.menu.menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Log.d("click item", item.toString())
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+
 
 
 }
